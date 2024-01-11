4802a482503d26121d27f0f2ba827d97fa31f4de
Lucas Jablon <lucas.jablon@gmail.com>
2024-01-10 17:17:43 -0800

    Map Implementation and null state fix

--- src/components/NavOptions.jsx
+++ src/components/NavOptions.jsx
@@ -1,4 +1,4 @@
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import {
   StyleSheet,
   Text,
@@ -10,6 +10,8 @@ import {
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
+import { selectOrigin } from "../redux/slices/navSlice";
+import { useSelector } from "react-redux";
 
 const data = [
   {
@@ -27,29 +29,31 @@ const data = [
 ];
 
 const NavOptions = () => {
-    const navigation = useNavigation();
-    return (
+  const navigation = useNavigation();
+  const origin = useSelector(selectOrigin);
+  
+  return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
         <TouchableOpacity
-          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 rounded-lg`
-
-          }
-          onPress={() => navigation.navigate(item.screen)}>
-          <View>
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40 rounded-lg`}
+          onPress={() => navigation.navigate(item.screen)}
+          disabled={!origin}
+        >
+          <View style={tw`${!origin && "opacity-20"}`}>
             <Image
               source={{ uri: item.image }}
               style={{ width: 120, height: 120, resizeMode: "contain" }}
             />
             <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
             <Icon
-            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
-                type="antdesign"
-                color="white"
-                name="arrowright"
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              type="antdesign"
+              color="white"
+              name="arrowright"
             />
           </View>
         </TouchableOpacity>
